1fa105076a03256b0e0896d2aa2ee5ecdabfefd3
ashwini21 <21ashwinirao@gmail.com>
2017-08-08 20:52:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('article one is sent');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
