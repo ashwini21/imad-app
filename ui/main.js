19dfdf0ff29ca3cc66b5b390648b0f33a5c69d41
ashwini21 <21ashwinirao@gmail.com>
2017-08-09 09:43:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,14 @@ console.log('Loaded!');
 
 var element=document.getElementById('val');
 element.innerHTML='new value';
+
+var img=document.getElementById('modi');
+
+var marginLeft=0;
+function moveRight (){
+    marginLeft=marginLeft+5;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onClick=function(){
+    var interval=setInterval(moveRight,50);
+};
